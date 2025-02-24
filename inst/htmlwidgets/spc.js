f0bb9749f14c86cb91627f17bf8fc7e87ab813e6
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2025-02-24 20:09:27 +0800

    Cleanup

--- inst/htmlwidgets/spc.js
+++ inst/htmlwidgets/spc.js
@@ -2,82 +2,65 @@ HTMLWidgets.widget({
   name: 'spc',
   type: 'output',
   factory: function(el, width, height) {
-  var ct_sel = new crosstalk.SelectionHandle();
-  var options_constructor = make_constructor(el);
-  var visual = new spc.Visual(options_constructor);
+    var ct_sel = new crosstalk.SelectionHandle();
+    var options_constructor = make_constructor(el);
+    var visual = new spc.Visual(options_constructor);
 
-  visual.selectionManager.getSelectionIds = () => {
-        return ct_sel.value ?? []
-      }
-  visual.selectionManager.clear = () => {
-    ct_sel.clear()
-  }
-  ct_sel.on("change", function(e) {
-    visual.updateHighlighting()
-  });
+    visual.selectionManager.getSelectionIds = () => ct_sel.value ?? []
+    visual.selectionManager.clear = () => ct_sel.clear()
+    ct_sel.on("change", function(e) { visual.updateHighlighting() });
 
-  visual.svg
-        .append("g")
-        .classed("tooltip", true);
+    visual.svg.append("g").classed("tooltip", true);
 
-  var options_update = {
-    dataViews: [
-        {
-            categorical: {
-                categories: [{
-                    source: { roles: {"key": true}},
-                    values: ["A", "B", "C"]
-                }],
-                values: [
-                    {
-                        source: { roles: {"numerators": true}},
-                        values: [1, 2, 3]
-                    }
-                ]
-            }
+    var options_update = {
+      dataViews: [{
+        categorical: {
+          categories: [{
+            source: { roles: {"key": true}},
+            values: ["A", "B", "C"]
+          }],
+          values: [{
+            source: { roles: {"numerators": true}},
+            values: [1, 2, 3]
+          }]
         }
-    ] ,
-    viewport: {
-        "width":width,
-        "height":height
-    },
-    type: 2
-};
+      }],
+      viewport: {
+        width: width,
+        height: height
+      },
+      type: 2
+    };
 
     return {
-
       renderValue: function(x) {
-        var data_keys = x.keys;
         var crosstalk_keys = x.settings.crosstalk_keys;
         ct_sel.setGroup(x.settings.crosstalk_group);
         visual.host.createSelectionIdBuilder = () => ({
-            withCategory: (cat, idx) => ({
-              createSelectionId: () => [crosstalk_keys[idx]]
-            })
+          withCategory: (cat, idx) => ({
+            createSelectionId: () => [crosstalk_keys[idx]]
           })
-        visual.selectionManager.select = (identity, multi) => {
+        })
+        visual.selectionManager.select = (identity, multi_select) => {
           var new_idents = [identity];
-          if (multi && ct_sel.value) {
+          if (multi_select && ct_sel.value) {
             new_idents = new_idents.concat(ct_sel.value)
           }
           ct_sel.set(new_idents)
           return { then: (f) => f() }
         }
+
         options_update.dataViews[0].categorical.categories[0].values = x.keys;
         options_update.dataViews[0].categorical.values[0].values = x.numerators;
 
         visual.update(options_update);
-
       },
 
       resize: function(width, height) {
-
         options_update.viewport.width = width;
         options_update.viewport.height = height;
         visual.update(options_update);
-      },
-
-      visual: visual
+      }
     };
   }
 });
