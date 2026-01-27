ec337e5f891a57736896e04445c0dfb73187e824
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2026-01-27 13:31:51 +0800

    Minify sources at installation if jsutils installed

---
+++ R/aaa.R
@@ -0,0 +1,25 @@
+minify_deps <- function(script) {
+  src_folder <- dirname(script)
+  src_file <- basename(script)
+  new_file <- gsub(".js", ".min.js", src_file, fixed = TRUE)
+  output_file <- file.path(src_folder, new_file)
+  if (!isTRUE(options("controlcharts.debug")) && requireNamespace("jsutils", quietly = TRUE)) {
+    terser_options <- list(
+      compress = list(dead_code = TRUE, drop_console = FALSE, passes = 2),
+      mangle = TRUE,
+      format = list(comments = FALSE)
+    )
+    minified <- jsutils::terser(script, terser_options)$code
+    writeLines(minified, output_file)
+  } else {
+    file.copy(script, output_file, overwrite = TRUE)
+  }
+}
+
+# If the `jsutils` package is installed, minify the JS dependencies during installation
+minify_deps(system.file("js", "minidom.js", package = "controlcharts"))
+minify_deps(system.file("htmlwidgets", "lib", "PBIFUN", "PBIFUN.js", package = "controlcharts"))
+minify_deps(system.file("htmlwidgets", "lib", "PBISPC", "PBISPC.js", package = "controlcharts"))
+minify_deps(system.file("htmlwidgets", "lib", "UTILS", "commonUtils.js", package = "controlcharts"))
+minify_deps(system.file("htmlwidgets", "lib", "UTILS", "headlessUtils.js", package = "controlcharts"))
+minify_deps(system.file("htmlwidgets", "lib", "UTILS", "interactiveUtils.js", package = "controlcharts"))
